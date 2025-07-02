3b1676c73f65a0dcfb29121f2bdab9c40deec07b
kimeunseo <167689080+kimeunseo007@users.noreply.github.com>
2025-07-02 11:05:25 +0900

    commit

---
+++ writer/src/main/java/miniproject/domain/PubApproveCommand.java
@@ -0,0 +1,18 @@
+package miniproject.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.Data;
+
+@Data
+public class PubApproveCommand {
+
+    private Long writerId;
+    private Long bookId;
+    private String title;
+    private String content;
+    private String coverUrl;
+    private String title;
+    private String content;
+    private String coverUrl;
+}
---
+++ writer/src/main/java/miniproject/domain/PublishRequested.java
@@ -0,0 +1,20 @@
+package miniproject.domain;
+
+import java.util.*;
+import lombok.*;
+import miniproject.domain.*;
+import miniproject.infra.AbstractEvent;
+
+@Data
+@ToString
+public class PublishRequested extends AbstractEvent {
+
+    private Long bookId;
+    private String title;
+    private String content;
+    private Long writerId;
+    private String coverUrl;
+    private String content;
+    private Long writerId;
+    private String coverUrl;
+}
